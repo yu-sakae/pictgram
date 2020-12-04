61aa128a16c7e4c28c55b1d2f2aeabbe6b11b15a
”阪衛悠” <nicecream03@gmail.com>
2020-12-04 15:02:52 +0000

    2020.12.5

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   
   resources :users
   resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+
 end
