313aa5989a9ca181755fefb211286614a8d7b19e
”阪衛悠” <nicecream03@gmail.com>
2020-12-17 09:38:08 +0000

    1217-1838

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
     if user && user.authenticate(session_params[:password])
       log_in user
-      redirect_to root_path, success: 'ログインに成功しました'
+      redirect_to topics_index_path, success: 'ログインに成功しました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
